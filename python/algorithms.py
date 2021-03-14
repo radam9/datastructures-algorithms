5dbdd45fa510033ce176f44e2794dee6f9d7aba0
Adam Saleh <radam9@gmail.com>
2021-03-14 10:49:47 +0100

    add selection_sort

--- python/algorithms.py
+++ python/algorithms.py
@@ -6,14 +6,14 @@ unordered_array = random.sample(range(limit), limit)
 search_value = random.randint(0, limit)
 
 
-def linearsearch(array, search_value):
+def linear_search(array, search_value):
     for index, element in enumerate(array):
         if element == search_value:
             return index
     return None
 
 
-def binarysearch(array, search_value):
+def binary_search(array, search_value):
     lower_bound = 0
     upper_bound = len(array) - 1
 
@@ -31,7 +31,7 @@ def binarysearch(array, search_value):
     return None
 
 
-def bubblesort(array):
+def bubble_sort(array):
     last_index = len(array) - 1
 
     sorting = True
@@ -43,5 +43,20 @@ def bubblesort(array):
                 array[index], array[index + 1] = array[index + 1], array[index]
                 sorting = True
         last_index -= 1
+
+    return array
+
+
+def selection_sort(array):
+    array_length = len(array)
+    for index in range(array_length - 1):
+        lowest_index = index
+
+        for index_two in range(index + 1, array_length):
+            if array[index_two] < array[lowest_index]:
+                lowest_index = index_two
+
+        if lowest_index != index:
+            array[index], array[lowest_index] = array[lowest_index], array[index]
 
     return array
