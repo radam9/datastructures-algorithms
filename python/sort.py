290efc8588a40686065e0294c3490552f867e1e3
Adam Saleh <radam9@gmail.com>
2021-03-20 10:58:05 +0100

    add quicksort

--- python/sort.py
+++ python/sort.py
@@ -5,6 +5,7 @@ unordered_array = random.sample(range(limit), limit)
 
 
 def bubble_sort(array):
+    """Sort array in place using Bubble Sort"""
     last_index = len(array) - 1
 
     sorting = True
@@ -17,10 +18,9 @@ def bubble_sort(array):
                 sorting = True
         last_index -= 1
 
-    return array
-
 
 def selection_sort(array):
+    """Sort array in place using Selection Sort"""
     array_length = len(array)
     for index in range(array_length - 1):
         lowest_index = index
@@ -32,10 +32,9 @@ def selection_sort(array):
         if lowest_index != index:
             array[index], array[lowest_index] = array[lowest_index], array[index]
 
-    return array
-
 
 def insertion_sort(array):
+    """Sort array in place using Instertion Sort"""
     array_length = len(array)
     for index in range(1, array_length):
         carry_value = array[index]
@@ -49,4 +48,28 @@ def insertion_sort(array):
                 break
         array[position + 1] = carry_value
 
-    return array
+
+def _partition(array, right_pointer, left_pointer):
+    pivot = array[right_pointer]
+    index = left_pointer - 1
+
+    for pointer in range(left_pointer, right_pointer):
+        if array[pointer] < pivot:
+            index += 1
+            array[index], array[pointer] = array[pointer], array[index]
+    array[index + 1], array[right_pointer] = array[right_pointer], array[index + 1]
+    return index + 1
+
+
+def quicksort(array, right_pointer=None, left_pointer=0):
+    """Sort array in place using QuickSort"""
+    if not right_pointer:
+        right_pointer = len(array) - 1
+
+    if right_pointer <= left_pointer:
+        return
+
+    pivot_index = _partition(array, right_pointer, left_pointer)
+
+    quicksort(array, pivot_index - 1, left_pointer)
+    quicksort(array, right_pointer, pivot_index + 1)
